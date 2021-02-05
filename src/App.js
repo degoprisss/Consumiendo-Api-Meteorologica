72d44facac11d8f4677f5bd182cd2a21995729a1
Diego Mesa <degoprisss@gmail.com>
2021-02-05 09:45:57 -0500

    Mi primera versión

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [ count, setCount ] = useState(0);
+  
+  useEffect(() => {
+    document.title = `Cambio de title ${count}`
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>{count}</h3>
+      <button onClick={() => setCount(count + 1)}>Aumer un numero</button>
     </div>
   );
 }
