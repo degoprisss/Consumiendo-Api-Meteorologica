f5b4416e1d1f57bfed8586d507e72eafdc74afdd
Diego Mesa <degoprisss@gmail.com>
2021-02-09 14:28:33 -0500

    a

--- src/App.js
+++ src/App.js
@@ -8,33 +8,33 @@ import React, { useEffect, useState } from "react";
 
 
 function App() {
-  let lat;
-  let long;
-  let saveLo = [];
+
   const [data, setData] = useState(undefined)
+  const [loca, setLoca] = useState([9.273344, -74.6422272])
   
-    navigator.geolocation.getCurrentPosition((props) => {
-      lat = props.coords.latitude;
-      long = props.coords.longitude;
-      saveLo = [lat, long];
-      });
-  
+
+  navigator.geolocation.watchPosition((props) => {
+    setLoca([props.coords.latitude, props.coords.longitude])
+  });
+
+
   useEffect(() => {
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${saveLo[0]}&lon=${saveLo[1]}&appid=e83dfacbf543995a2d9e1b6aae5bb027&units=metric`
+  
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${loca[0]}&lon=${loca[1]}&appid=e83dfacbf543995a2d9e1b6aae5bb027&units=metric`
     fetch(url, { mode: "cors" })
-    .then((get) => get.json())
-    .then((data) => {
-      setData(data) 
-    })
+      .then((get) => get.json())
+      .then((data) => {
+        setData(data)
+      })
   }, []);
 
   return (
     <div className="container">
-      <div className='container-card col-lg-5 col-md-12 col-sm-12'>{data === undefined ? 'Cargando': 
-      <Location location={data}/>}  
-        <div className='icono-temperature'>{data === undefined ? 'Cargando': 
-        <Temperature temperature={data}/>
-        } {data === undefined ? 'Cargando': <RestInformation restInformation={data}/>}   
+      <div className='container-card col-lg-5 col-md-12 col-sm-12'>{data === undefined ? 'Cargando' :
+        <Location location={data} />}
+        <div className='icono-temperature'>{data === undefined ? 'Cargando' :
+          <Temperature temperature={data} />
+        } {data === undefined ? 'Cargando' : <RestInformation restInformation={data} />}
         </div>
       </div>
     </div>
