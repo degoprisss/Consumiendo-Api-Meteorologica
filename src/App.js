bf501bd10b88f039f81586e9465bbb0003673033
Diego Mesa <degoprisss@gmail.com>
2021-02-09 22:53:11 -0500

    Button

--- src/App.js
+++ src/App.js
@@ -4,31 +4,37 @@ import './Grid-portafolio.css';
 import Location from './RenderLocation.js'
 import Temperature from './temperature.js'
 import RestInformation from './RenderInfo'
+import ButtonLocation from './button.js'
 import React, { useEffect, useState } from "react";
 
 
 function App() {
   let lat;
   let long;
-  let saveLocation  = [lat, long];
+  // let saveLocation  = [lat, long];
   const [data, setData] = useState(undefined)
+  const [saveLo, setSaveLo] = useState([0, 0])
   
+  const handlerLocation = () => {
   navigator.geolocation.getCurrentPosition((props) => {
-    lat = props.coords.latitude;
-    long = props.coords.longitude;
-    saveLocation = [lat, long]
+   lat = props.coords.latitude;
+   long = props.coords.longitude;
+  //   saveLocation = [lat, long]
+  setSaveLo([lat, long])
   }); 
+}
+
   
 
   useEffect(() => {
 
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${saveLocation[0]}&lon=${saveLocation[1]}&appid=e83dfacbf543995a2d9e1b6aae5bb027&units=metric`
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${saveLo[0]}&lon=${saveLo[1]}&appid=e83dfacbf543995a2d9e1b6aae5bb027&units=metric`
     fetch(url, { mode: "cors" })
       .then((get) => get.json())
       .then((data) => {
         setData(data)
       })
-  }, []);
+  }, [saveLo]);
 
   return (
     <div className="container">
@@ -38,6 +44,7 @@ function App() {
           <Temperature temperature={data} />
         } {data === undefined ? 'Cargando' : <RestInformation restInformation={data} />}
         </div>
+        <ButtonLocation location={handlerLocation}/>
       </div>
     </div>
   );
---
+++ src/button.js
@@ -0,0 +1,9 @@
+const ButtonLocation = (props) => {
+    return (
+        <div class="container-button">
+        <button className="change" onClick={props.location}>Buscar mi ubicación</button>
+        </div>
+    )
+}
+
+export default ButtonLocation;
